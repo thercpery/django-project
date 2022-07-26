af1f08b838e9264e1d0700028f14064d3693650c
Roy Christian Pery <thercpery@gmail.com>
2022-07-26 10:19:30 +0800

    added search page

--- listings/views.py
+++ listings/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .choices import price_choices, bedroom_choices, state_choices
 from .models import Listing
 
 
@@ -17,7 +18,19 @@ def index(request):
     return render(request, "listings/listings.html", context) 
 
 def listing(request, listing_id):
-    return render(request, "listings/listing.html") 
+    listing = get_object_or_404(Listing, pk=listing_id)
+
+    context = {
+        "listing": listing
+    }
+
+    return render(request, "listings/listing.html", context) 
 
 def search(request):
-    return render(request, "listings/search.html") 
+    context = {
+        "state_choices": state_choices,
+        "bedroom_choices": bedroom_choices,
+        "price_choices": price_choices,
+    }
+
+    return render(request, "listings/search.html", context) 
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from listings.models import Listing
+from listings.choices import price_choices, bedroom_choices, state_choices
 from realtors.models import Realtor
 
 
@@ -9,7 +10,10 @@ def index(request):
     listings = Listing.objects.order_by("-list_date").filter(is_published=True)[:3]
     
     context = {
-        "listings": listings
+        "listings": listings,
+        "state_choices": state_choices,
+        "bedroom_choices": bedroom_choices,
+        "price_choices": price_choices,
     }
     return render(request, "pages/index.html", context)
 
