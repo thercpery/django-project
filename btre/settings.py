85f0fd11f82242e1346f333cb741ac6ae16a8caa
Roy Christian Pery <thercpery@gmail.com>
2022-07-28 13:44:24 +0800

    added local settings import

--- btre/settings.py
+++ btre/settings.py
@@ -163,3 +163,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ.get("GMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("GMAIL_HOST_PASS")
 EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
